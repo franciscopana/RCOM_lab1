4e8b38a4768cc0e0f35fa16ffa7de4d841cd5dfa
Francisco Ana <franciscopdana@gmail.com>
2023-11-10 14:14:59 +0000

    64 maxpayload

--- src/application_layer.c
+++ src/application_layer.c
@@ -9,7 +9,7 @@
 #include <time.h>
 
 
-#define MAX_PAYLOAD 256
+#define MAX_PAYLOAD 64
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename){
